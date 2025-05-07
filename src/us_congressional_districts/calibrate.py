c23539e2eb323af1f010905652256c1466bcc1e0
baogorek <baogorek@gmail.com>
2025-05-07 09:00:27 -0400

    national, state, district logic

--- src/us_congressional_districts/calibrate.py
+++ src/us_congressional_districts/calibrate.py
@@ -1,11 +1,9 @@
 import os
-#import argparse
 from pathlib import Path
 
 import pandas as pd
 import numpy as np
 import torch
-# from tqdm import tqdm
 import h5py
 from policyengine_us import Microsimulation
 
@@ -14,8 +12,7 @@ from us_congressional_districts.utils import get_data_directory
 
 def create_district_target_matrix(
     dataset: str = "cps_2022",
-    time_period: int = 2022,
-    reform=None,
+    time_period: int = 2022
 ):
     ages = pd.read_csv(
         get_data_directory() / "input" / "demographics" / "age-district.csv"
@@ -44,10 +41,103 @@ def create_district_target_matrix(
 
         y[f"age/{age_range}"] = ages[age_range]
 
-    # Mask so that weights cannot be shared among households of different states
+    # Weights should not be shared among households of different states
     state_mask = create_state_mask(
         household_states = sim.calculate("state_fips").values,
         districts=ages.GEO_ID
+    )
+
+    return matrix, y, state_mask
+
+# NOTE: these next two functions are maybe a little goofy because they're the
+# exact same as the district_target_matrix function except for the target
+def create_national_target_matrix(
+    dataset: str = "cps_2022",
+    time_period: int = 2022
+):
+    ages = pd.read_csv(
+        get_data_directory() / "input" / "demographics" / "age-national.csv"
+    )
+
+    # Only to get the district codes for use with the mask!
+    ages_districts = pd.read_csv(
+        get_data_directory() / "input" / "demographics" / "age-district.csv"
+    )
+    district_codes = ages_districts['GEO_ID']
+
+    ages_count_matrix = ages.iloc[:, 2:]
+    age_ranges = list(ages_count_matrix.columns)
+
+    sim = Microsimulation(dataset=dataset)
+    sim.default_calculation_period = time_period
+
+    matrix = pd.DataFrame()
+    y = pd.DataFrame()
+
+    age = sim.calculate("age").values
+    for age_range in age_ranges:
+        if age_range != "85+":
+            lower_age, upper_age = age_range.split("-")
+            in_age_band = (age >= int(lower_age)) & (age < int(upper_age))
+        else:
+            in_age_band = age >= 85 
+
+        matrix[f"age/{age_range}"] = sim.map_result(
+            in_age_band, "person", "household"
+        )
+
+        y[f"age/{age_range}"] = ages[age_range]
+
+    # Weights should not be shared among households of different states
+    state_mask = create_state_mask(
+        household_states = sim.calculate("state_fips").values,
+        districts=district_codes
+    )
+
+    return matrix, y, state_mask
+
+# NOTE: see above
+def create_state_target_matrix(
+    dataset: str = "cps_2022",
+    time_period: int = 2022
+):
+    ages = pd.read_csv(
+        get_data_directory() / "input" / "demographics" / "age-state.csv"
+    )
+
+    # Only to get the district codes for use with the mask!
+    ages_districts = pd.read_csv(
+        get_data_directory() / "input" / "demographics" / "age-district.csv"
+    )
+    district_codes = ages_districts['GEO_ID']
+
+    ages_count_matrix = ages.iloc[:, 2:]
+    age_ranges = list(ages_count_matrix.columns)
+
+    sim = Microsimulation(dataset=dataset)
+    sim.default_calculation_period = time_period
+
+    matrix = pd.DataFrame()
+    y = pd.DataFrame()
+
+    age = sim.calculate("age").values
+    for age_range in age_ranges:
+        if age_range != "85+":
+            lower_age, upper_age = age_range.split("-")
+            in_age_band = (age >= int(lower_age)) & (age < int(upper_age))
+        else:
+            in_age_band = age >= 85 
+
+        matrix[f"age/{age_range}"] = sim.map_result(
+            in_age_band, "person", "household"
+        )
+
+        y[f"age/{age_range}"] = ages[age_range]
+
+    # Weights should not be shared among households of different states
+    state_mask = create_state_mask(
+        household_states = sim.calculate("state_fips").values,
+        districts=district_codes
     )
 
     return matrix, y, state_mask
@@ -77,6 +167,14 @@ def calibrate(
         "cps_2022", 2022
     )
 
+    m_national_, y_national_, state_mask_national_ = create_national_target_matrix(
+        "cps_2022", 2022
+    )
+
+    m_state_, y_state_, state_mask_state_ = create_state_target_matrix(
+        "cps_2022", 2022
+    )
+
     sim = Microsimulation(dataset = "cps_2022")
     sim.default_calculation_period = 2022
 
@@ -85,6 +183,7 @@ def calibrate(
     original_weights = np.log(
         sim.calculate("household_weight", 2022).values / COUNT_DISTRICTS
     )
+    # TODO: In what sense are there multiple sets of weights?
     weights = torch.tensor(
         np.ones((COUNT_DISTRICTS, len(original_weights)))
         * original_weights,
@@ -94,12 +193,36 @@ def calibrate(
 
     metrics = torch.tensor(matrix_.values, dtype=torch.float32)
     y = torch.tensor(y_.values, dtype=torch.float32)
+    matrix_national = torch.tensor(m_national_.values, dtype=torch.float32)
+    y_national = torch.tensor(y_national_.values, dtype=torch.float32)
+    matrix_state = torch.tensor(m_state_.values, dtype=torch.float32)
+    y_state = torch.tensor(y_state_.values, dtype=torch.float32)
+
+    # NOTE: I guess I'm learning that the state mask only needs to be computed once
+    # Really the metric matrix too
     r = torch.tensor(state_mask, dtype=torch.float32)
 
     def loss(w):
         pred = (w.unsqueeze(-1) * metrics.unsqueeze(0)).sum(dim=1)
         mse = torch.mean(((pred - y) / y) ** 2)
-        return mse
+
+        pred_n = (w.sum(axis=0) * matrix_national.T).sum(axis=1)
+        mse_n = torch.mean(((pred_n - y_national) / y_national) ** 2)
+
+        # State level - create aggregated metrics for each state
+        # Create district to state map. TODO: put real logic in here 
+        num_states = 50
+        districts_per_state = 435 // num_states  # How many districts to assign to each state
+        
+        pred_s = torch.zeros(num_states, pred.size(1)) #, device=pred.device)
+        for s in range(num_states):
+            start_idx = s * districts_per_state
+            end_idx = start_idx + districts_per_state if s < num_states - 1 else 435
+            
+            pred_s[s] = pred[start_idx:end_idx].sum(dim=0)
+        mse_s = torch.mean(((pred_s - y_state) / y_state) ** 2)
+
+        return mse + mse_n + mse_s
 
     optimizer = torch.optim.Adam([weights], lr=0.15)
 
